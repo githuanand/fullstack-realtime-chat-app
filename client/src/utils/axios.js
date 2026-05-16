ed0e3542813ef0727a3bd9f34fe03f977291f7ab
githuanand <aj1001194@gmail.com>
2026-05-16 15:41:12 +0530

    Updated axios for Vercel environment variable

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,18 +1,19 @@
 import axios from "axios";
 
-
 const instance = axios.create({
-  baseURL: "https://fullstack-realtime-chat-backend.onrender.com",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://127.0.0.1:8000",
 });
 
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
-const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-if (token) {
-  instance.defaults.headers.common[
-    "Authorization"
-  ] = `Bearer ${token}`;
-}
-
+  return config;
+});
 
 export default instance;
